b2b71a05acbed499dd8cac7b16afe75b0a4019f7
Victor Kemboi <vickemtoo97@gmail.com>
2024-03-19 14:42:47 +0300

    Using Arrow Function

---
+++ index.js
@@ -0,0 +1,11 @@
+const num1=2000;
+const num2=100;
+const divide=(num1,num2) =>{
+    return num1/num2;
+}
+const square=(num2) =>{
+    return num2*num2;
+}
+const add=(num1,num2) => {
+    return num1+num2;
+}
